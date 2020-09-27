ab010112c2ef315cad6f40278438f9e7a54e39bd
Mike Cluck <mike-cluck@pluralsight.com>
2020-09-27 15:06:32 -0600

    Add series of operators Function composition Unary Postfix Parenthesized expressions

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,6 @@
 import { Lexer } from './lexer'
 import { S, TokenType } from './types'
-import { badToken, matchToken } from './util'
+import { badToken, matchToken, todo } from './util'
 
 export const expression = (input: string): S => {
   const lexer = new Lexer(input)
@@ -13,6 +13,26 @@ export const expressionBindingPower = (
 ): S => {
   let lhs = matchToken<S>(lexer.next(), {
     Atom: (token) => S.Atom(token.value),
+    Op: (token) => {
+      if (token.value === '(') {
+        const lhs = expressionBindingPower(lexer, 0)
+        const next = lexer.next()
+        return matchToken<S>(next, {
+          Op: (token) => {
+            if (token.value !== ')') {
+              throw new Error(`Bad op: ${token.value}`)
+            }
+            return lhs
+          },
+          _: badToken,
+        })
+      }
+
+      const op = token.value
+      const [, rightBindingPower] = prefixBindingPower(op)
+      const rhs = expressionBindingPower(lexer, rightBindingPower)
+      return S.Cons(op, [rhs])
+    },
     _: badToken,
   })
 
@@ -26,21 +46,64 @@ export const expressionBindingPower = (
       Op: (token) => token.value,
       _: badToken,
     })
-    const [leftBindingPower, rightBindingPower] = infixBindingPower(op)
-    if (leftBindingPower < minBindingPower) {
-      break
+
+    const postfix = postfixBindingPower(op)
+    if (postfix !== null) {
+      const [leftBindingPower] = postfix
+      if (leftBindingPower < minBindingPower) {
+        break
+      }
+
+      lexer.next()
+
+      if (op === '[') {
+        const rhs = expressionBindingPower(lexer, 0)
+        const next = lexer.next()
+        if (next.type === TokenType.Op && next.value !== ']') {
+          throw new Error(`Bad op: ${next.value}`)
+        } else if (next.type !== TokenType.Op) {
+          badToken(next)
+        }
+
+        lhs = S.Cons(op, [lhs, rhs])
+      } else {
+        lhs = S.Cons(op, [lhs])
+      }
+      continue
     }
 
-    lexer.next()
-    const rhs = expressionBindingPower(lexer, rightBindingPower)
+    const infix = infixBindingPower(op)
+    if (infix !== null) {
+      const [leftBindingPower, rightBindingPower] = infix
+      if (leftBindingPower < minBindingPower) {
+        break
+      }
 
-    lhs = S.Cons(op, [lhs, rhs])
+      lexer.next()
+      const rhs = expressionBindingPower(lexer, rightBindingPower)
+
+      lhs = S.Cons(op, [lhs, rhs])
+      continue
+    }
+
+    break
   }
 
   return lhs
 }
 
-const infixBindingPower = (op: string): [number, number] => {
+const prefixBindingPower = (op: string): [undefined, number] => {
+  switch (op) {
+    case '+':
+    case '-':
+      return [undefined, 5]
+
+    default:
+      throw new Error(`Bad op: ${op}`)
+  }
+}
+
+const infixBindingPower = (op: string): [number, number] | null => {
   switch (op) {
     case '+':
     case '-':
@@ -50,7 +113,21 @@ const infixBindingPower = (op: string): [number, number] => {
     case '/':
       return [3, 4]
 
+    case '.':
+      return [10, 9]
+
     default:
-      throw new Error(`Bad op: ${op}`)
+      return null
+  }
+}
+
+const postfixBindingPower = (op: string): [number, undefined] | null => {
+  switch (op) {
+    case '!':
+    case '[':
+      return [7, undefined]
+
+    default:
+      return null
   }
 }
--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -16,3 +16,37 @@ test('complex expression', () => {
   const s = expression('a + b * c * d + e')
   expect(format(s)).toBe('(+ (+ a (* (* b c) d)) e)')
 })
+
+test('function composition operator', () => {
+  let s = expression('f . g . h')
+  expect(format(s)).toBe('(. f (. g h))')
+
+  s = expression('1 + 2 + f . g . h * 3 * 4')
+  expect(format(s)).toBe('(+ (+ 1 2) (* (* (. f (. g h)) 3) 4))')
+})
+
+test('unary operators', () => {
+  let s = expression('--1 * 2')
+  expect(format(s)).toBe('(* (- (- 1)) 2)')
+
+  s = expression('--f . g')
+  expect(format(s)).toBe('(- (- (. f g)))')
+})
+
+test('postfix operator', () => {
+  let s = expression('-9!')
+  expect(format(s)).toBe('(- (! 9))')
+
+  s = expression('f . g !')
+  expect(format(s)).toBe('(! (. f g))')
+})
+
+test('parenthesized expressions', () => {
+  const s = expression('(((0)))')
+  expect(format(s)).toBe('0')
+})
+
+test('array indexing', () => {
+  const s = expression('x[0][1]')
+  expect(format(s)).toBe('([ ([ x 0) 1)')
+})
